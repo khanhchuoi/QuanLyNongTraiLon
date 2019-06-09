cd6ca04428c3ea78a5ed3b91af9afbea644c4188
Khanh Dev <kieukhanhudpm@gmail.com>
2019-06-09 19:19:10 +0700

    Remove newfolder chứa project cũ bị lỗi system.Web

--- New folder/QuanLyNongTraiLonThit.TienIch/Properties/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuanLyNongTraiLonThit.TangGiaoDien.MvcApplication" Language="C#" %>
--- New folder/QuanLyNongTraiLonThit/Assets/js/getquantitycart.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function () {
-    $('#addCart').click(function () {
-        var quantityCart = parseInt($('#QuantityCart').val());
-        var url = "/ShoppingCart/AddToCart?bookId=@Model.Book.Id" + "&quantity=" + quantityCart;
-        $('#addCart').attr("href", url);
-    });
-});
